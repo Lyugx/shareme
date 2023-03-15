5d2f1df36f5a40b54f39ce7bcf638e446f122434
Gaoxiang Lyu <gaoxianglyu@gmail.com>
2023-03-15 19:12:45 -0400

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+export const schemaTypes = [user,pin,comment,postedBy,save]
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
